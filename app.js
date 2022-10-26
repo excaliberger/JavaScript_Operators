7738e255c73c3155eadc9abc68e1a0d137d0b1e6
Emily Berger <excaliberger@gmail.com>
2022-10-26 18:13:09 -0400

    exercise 1- 3

--- app.js
+++ app.js
@@ -5,15 +5,53 @@ console.log(
 
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+console.log(a);
+console.log(b);
+console.log(add);
+console.log(minus);
+console.log(multiply);
+console.log(dividing);
 
-// YOUR CODE HERE
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+console.log(num + str);
+console.log(num + str2);
+console.log(num + isPresent);
+console.log(firstName + num);
+console.log(isPresent + str);
+console.log(firstName + lastName);
+
+
+
 
 // YOUR CODE HERE
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
-
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+console.log(val == str3);
+console.log(val === str3);
+console.log(!isPresent2);
+console.log("eleven" == str4 && val >= str3);
+console.log(!isPresent || isPresent2);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log(0 !== false);
 // YOUR CODE HERE
